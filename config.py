8b94223ec02aa9f26447a8ca1cea252324b3bfa9
Yuriiiptv <reseller72@gmail.com>
2025-05-06 15:04:54 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,32 @@
+BOT_TOKEN = '7124135847:AAFkfYgmVZRAEic8NGzN4NOqb4VxsoC-DIc'
+
+CHANNEL_NAMES = [
+    'Первый канал HD',
+    'Россия 1 HD',
+    'НТВ HD',
+    'НТВ Мир',
+    'ТВЦ HD',
+    'Пятый канал',
+    'ТНТ HD',
+    'СТС HD',
+    'СТС Love',
+    'Рен ТВ HD',
+    'Мир ТВ HD',
+    'ОТР',
+    'Звезда HD',
+    'Че',
+    'ТВ3',
+    'ТВ 3 +2',
+    'ТВ3 +4',
+    'ТВ3 +6',
+    'Пятница',
+    'Пятница HD',
+    'Суббота!',
+    'Домашний HD'
+]
+
+M3U_URLS = [
+    'https://iptv-org.github.io/iptv/languages/rus.m3u',
+    'https://raw.githubusercontent.com/freearhey/iptv/master/channels/ru.m3u',
+    'https://raw.githubusercontent.com/iptv-restream/IPTV/master/streams/ru.m3u'
+]
