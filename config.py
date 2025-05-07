9e459fe368f5c09e28a37d928d76a869952150f1
Yuriiiptv <reseller72@gmail.com>
2025-05-07 10:58:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -26,8 +26,8 @@ CHANNEL_NAMES = [
 ]
 
 M3U_URLS = [
-    'https://smolnp.github.io/IPTVru/IPTVru.m3u'
-    'https://smolnp.github.io/IPTVru/IPTVstable.m3u8'
-    'https://iptv-org.github.io/iptv/countries/ru.m3u'
+    'https://smolnp.github.io/IPTVru/IPTVru.m3u',
+    'https://smolnp.github.io/IPTVru/IPTVstable.m3u8',
+    'https://iptv-org.github.io/iptv/countries/ru.m3u',
     'https://raw.githubusercontent.com/blackbirdstudiorus/IPTVPlay/main/IPTVPlay.m3u'
 ]
